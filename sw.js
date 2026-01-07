13930bdcec794fe313e5fa707525822b04ed6c39
gerenciamentodecontasorlando-create <gerenciamentodecontasorlando@gmail.com>
2026-01-07 09:10:34 -0300

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,41 @@
+const CACHE_NAME = "agenda-pwa-v1.0.0";
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./styles.css",
+  "./app.js",
+  "./manifest.json",
+  "./icons/icon-192.png",
+  "./icons/icon-512.png"
+];
+
+// Instala
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+// Ativa
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.map(k => (k === CACHE_NAME ? null : caches.delete(k))))
+    )
+  );
+  self.clients.claim();
+});
+
+// Fetch
+self.addEventListener("fetch", (event) => {
+  const req = event.request;
+  const url = new URL(req.url);
+
+  // Só trata o que é do mesmo origin
+  if (url.origin !== location.origin) return;
+
+  event.respondWith(
+    caches.match(req).then((cached) => cached || fetch(req))
+  );
+});
